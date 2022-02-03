c80c719f0cecce9fd5771ca9410aae044dfc7f0f
MrXacx <misterxacx@gmail.com>
2022-02-03 17:50:33 -0300

    Adiciona codigo php

--- index.php
+++ index.php
@@ -6,31 +6,22 @@ $categorias[] = "Adolescente";
 $categorias[] = "Adulto";
 
 $nome = "Ariel";
-$idade = 12;
+$idade = 23;
 
 if($idade >= 6 AND $idade <= 12){
-    for($i = 0; $i <= count($categorias); $i++){
-        if($categorias[$i] == "Infantil"){
-            echo "O competidor " , $nome , " participa da categoria " , $categorias[0];
-        }
-    }
+    echo "O competidor " , $nome , " participa da categoria " , $categorias[0];
 }
+
 if($idade >= 13 AND $idade <= 18 ){
-    for($i = 0; $i <= count($categorias); $I++){
-        if($categorias[$i] == "Adolescente"){
-            echo "O competidor " , $nome , " participa da categoria " , $categorias[0];
-        }
-    }
+    echo "O competidor " , $nome , " participa da categoria " , $categorias[1];
 }
+
 if($idade >= 19 AND $idade <= 45){
-    for($i = 0; $i <= count($categorias); $I++){
-        if($categorias[$i] == "Adulto"){
-            echo "O competidor " , $nome , " participa da categoria " , $categorias[0];
-        }
-    }
+    echo "O competidor " , $nome , " participa da categoria " , $categorias[2];
 }
-/*else{
+
+else{
     echo "Infelizmente a competição não comporta a sua faixa etária.";
-}*/
+}
 
 ?>
